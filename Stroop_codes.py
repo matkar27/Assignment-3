bc74853da7d3d53ac444bc06c16b7f46c996b089
matkar27 <78582561+matkar27@users.noreply.github.com>
2021-04-10 01:37:37 +0530

    Update Stroop_codes.py

--- Stroop_codes.py
+++ Stroop_codes.py
@@ -432,7 +432,17 @@ thisExp.addData('thanksText.stopped', thanksText.tStopRefresh)
 
 # Flip one final time so any remaining win.callOnFlip() 
 # and win.timeOnFlip() tasks get executed before quitting
-win.flip()       
+win.flip()     
+
+# these shouldn't be strictly necessary (should auto-save)
+thisExp.saveAsWideText(filename+'.csv', delim='auto')
+thisExp.saveAsPickle(filename)
+logging.flush()
+# make sure everything is closed down
+thisExp.abort()  # or data files will save again on exit
+win.close()
+core.quit()
+
                 
         
         
